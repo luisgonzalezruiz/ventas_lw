d19f919a5b6a152dc124eed46e3d49181203eb3a
Luis Gonzalez <luisgonzalezruiz1979@gmail.com>
2022-06-30 14:18:22 -0400

    permisos terminado 1

---
+++ app/Http/Livewire/Permiso/Permisos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Permiso;
+
+use Livewire\Component;
+
+class Permisos extends Component
+{
+    public function render()
+    {
+        return view('livewire.permiso.permisos');
+    }
+}
